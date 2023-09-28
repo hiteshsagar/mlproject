ab90ccefa110353cc40e2fc54f654308b46d387e
unknown <200528076@student.georgianc.on.ca>
2023-09-28 09:01:13 -0400

    errors resolved, artifacts created

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject',
-    version='0.0..1',
+    version='0.0.2',
     author='Hitesh',
     author_email='hiteshsagar.ai@gmail.com',
     packages=find_packages(),
